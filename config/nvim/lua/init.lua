e068741a5727cf69a95b99dbd2cbd9474ff625cb
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2025-03-01 17:10:16 +1000

    fix "="

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader= " "
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.number=true
+vim.opt.relativenumber=true
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
