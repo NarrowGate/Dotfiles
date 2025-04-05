a1b51df6fc0a114598d24db5c0b4aeba1e9fdca8
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2025-04-05 18:50:28 +1000

    Edit close buffer

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -16,7 +16,7 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
-vim.keymap.set('n', 'x', ':q<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'x', ':bd<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-\\>', ':vs<CR>', { noremap = true, silent = true })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
